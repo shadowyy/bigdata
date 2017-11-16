12821021891019629f0a7d5e20907c5abde243f8
yuyue <yuyue@weshare.com.cn>
2017-11-16 20:34:03 +0800

    python

--- src/test/python/test.py
+++ src/test/python/test.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env pyton
 # -- coding:utf-8 --
 
-from os.path import join as xx
-
-import mod1
-# import mod2.mod2 as xx
-# from mod2.mod2 import sayHello
-from mod2.mod2 import sayHello as sayHello11
-from mod3.mod3 import sayHello
-
-print mod1.__name__
-
-mod1.sayHello()
-
-print(xx('C:\\windows', 'system32'))
-sayHello11()
-
-sayHello()
+# from os.path import join as xx
+#
+# import mod1
+# # import mod2.mod2 as xx
+# # from mod2.mod2 import sayHello
+# from mod2.mod2 import sayHello as sayHello11
+# from mod3.mod3 import sayHello
+#
+# print mod1.__name__
+#
+# mod1.sayHello()
+#
+# print(xx('C:\\windows', 'system32'))
+# sayHello11()
+#
+# sayHello()
 
 # import os
 # print(os.path.join('C:\\windows', 'system32'))
 #
 # import os.path
 # print(os.path.join('C:\\windows', 'system32'))
+
+
+def funA(arg):
+    print 'A'
+
+
+@funA
+def funB():
+    print 'B'
+
+# funA(funB())
